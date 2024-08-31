ee5ed62776674b27ce6d188124781c11072f2e00
Tsutomu Nagura <nagura@d6.dion.ne.jp>
2024-09-01 06:05:53 +0900

    Supported GPT-4o/GPT-4o mini LLM

--- types/openai.ts
+++ types/openai.ts
@@ -8,61 +8,24 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  //GPT_3_5 = 'gpt-3.5-turbo',
-  //GPT_3_5_AZ = 'gpt-35-turbo',
-  //GPT_4 = 'gpt-4',
-  //GPT_4_32K = 'gpt-4-32k',
-  GPT_3_5_AZ_16K = 'gpt-35-turbo-16k',
-  GPT_4_AZ = 'gpt-4',
-  GPT_4_AZ_32K = 'gpt-4-32k',
-
+  GPT_4O_AZ = 'gpt-4o-2024-05-13',
+  GPT_4O_MINI_AZ = 'gpt-4o-mini-2024-07-18',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5_AZ_16K;
+export const fallbackModelID = OpenAIModelID.GPT_4O_MINI_AZ;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  //[OpenAIModelID.GPT_3_5]: {
-  //  id: OpenAIModelID.GPT_3_5,
-  //  name: 'GPT-3.5',
-  //  maxLength: 12000,
-  //  tokenLimit: 4000,
-  //},
-  //[OpenAIModelID.GPT_3_5_AZ]: {
-  //  id: OpenAIModelID.GPT_3_5_AZ,
-  //  name: 'GPT-3.5',
-  //  maxLength: 12000,
-  //  tokenLimit: 4000,
-  //},
-  //[OpenAIModelID.GPT_4]: {
-  //  id: OpenAIModelID.GPT_4,
-  //  name: 'GPT-4',
-  //  maxLength: 24000,
-  //  tokenLimit: 8000,
-  //},
-  //[OpenAIModelID.GPT_4_32K]: {
-  //  id: OpenAIModelID.GPT_4_32K,
-  //  name: 'GPT-4-32K',
-  //  maxLength: 96000,
-  //  tokenLimit: 32000,
-  //},
-  [OpenAIModelID.GPT_3_5_AZ_16K]: {
-    id: OpenAIModelID.GPT_3_5_AZ_16K,
-    name: 'GPT-3.5-16k',
-    maxLength: 48000,
-    tokenLimit: 16384,
+  [OpenAIModelID.GPT_4O_MINI_AZ]: {
+    id: OpenAIModelID.GPT_4O_MINI_AZ,
+    name: 'GPT-4o mini',
+    maxLength: 512000,
+    tokenLimit: 128000,
   },
-  [OpenAIModelID.GPT_4_AZ]: {
-    id: OpenAIModelID.GPT_4_AZ,
-    name: 'GPT-4-8k',
-    maxLength: 24000,
-    tokenLimit: 8000,
+  [OpenAIModelID.GPT_4O_AZ]: {
+    id: OpenAIModelID.GPT_4O_AZ,
+    name: 'GPT-4o',
+    maxLength: 512000,
+    tokenLimit: 128000,
   },
-  [OpenAIModelID.GPT_4_AZ_32K]: {
-    id: OpenAIModelID.GPT_4_AZ_32K,
-    name: 'GPT-4-32k',
-    maxLength: 96000,
-    tokenLimit: 32000,
-  },
-
 };
